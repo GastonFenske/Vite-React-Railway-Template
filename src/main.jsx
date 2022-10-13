f2fba543b8e926872b8269ab7aa74ff3e84d25b0
GastonFenske <thesacha2000f@gmail.com>
2022-10-13 14:46:47 -0300

    added hash router

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { HomeScreen } from './screens/HomeScreen'
 import { LadingPageScreen } from './screens/LadingPageScreen'
 import './styles.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { GastonFenskeApp } from './GastonFenskeApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GastonFenskeApp />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
